24c30f0c6d512648e2b8baa62a784010ae014b93
Jesse de Boer <jesse@frsc.nl>
2023-05-12 10:31:20 +0200

    Added MethodListExtended to set the preselected payment method when set

---
+++ Payment/MethodListExtended.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is provided with Magento in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * Copyright © MultiSafepay, Inc. All rights reserved.
+ * See DISCLAIMER.md for disclaimer details.
+ */
+
+declare(strict_types=1);
+
+namespace MultiSafepay\MagewireCheckout\Payment;
+
+use Hyva\Checkout\Model\Magewire\Component\EvaluationResultFactory;
+use Hyva\Checkout\Magewire\Checkout\Payment\MethodList;
+use Magento\Checkout\Model\Session as SessionCheckout;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Model\Quote\Payment;
+use Magento\Quote\Model\QuoteRepository;
+use MultiSafepay\ConnectCore\Config\Config;
+
+class MethodListExtend extends MethodList
+{
+    public ?string $method = null;
+
+    public function __construct(
+        SessionCheckout $sessionCheckout,
+        CartRepositoryInterface $cartRepository,
+        EvaluationResultFactory $evaluationResultFactory,
+        private readonly Config $config,
+        protected QuoteRepository $quoteRepository,
+    ) {
+        parent::__construct(
+            $sessionCheckout,
+            $cartRepository,
+            $evaluationResultFactory
+        );
+    }
+
+    /**
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    public function getPaymentFromQuote(): Payment
+    {
+        return $this->sessionCheckout->getQuote()->getPayment();
+    }
+
+    public function boot(): void
+    {
+        try {
+            $method = $this->sessionCheckout->getQuote()->getPayment()->getMethod();
+
+            if ($this->config->getPreselectedMethod() && !$method) {
+                $this->updatedMethod($this->config->getPreselectedMethod());
+                $method = $this->config->getPreselectedMethod();
+            }
+        } catch (LocalizedException $exception) {
+            $method = null;
+        }
+
+        $this->method = $method;
+    }
+}
